bfafbad992d3edb6a3224887d2c32aeccf784dd5
Daniel Polo Tobarra <daniel.polo@altran.es>
2019-04-11 18:23:19 +0200

    Añadimos archivo

--- rendez-vous1.py
+++ rendez-vous1.py
@@ -11,31 +11,30 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-R = 7144000                 #semimayor target axis (m)
-Rc =  7141000              #semimayor chaser init axis (m)
-mu = 3.9857128e14            #SI
-w = math.sqrt(mu/R**3)
-r0 = [-17000,0,3000]               #initial starting position
+R = 7144000  # semimayor target axis (m)
+Rc = 7141000  # semimayor chaser init axis (m)
+mu = 3.9857128e14  # SI
+w = math.sqrt(mu / R ** 3)
+r0 = [-17000, 0, 3000]  # initial starting position
 
 v_dif_alt = 1.5 * w * r0[2]
 
-rdot0 = [v_dif_alt,0,0]          #initial velocity  (m/s)
+rdot0 = [v_dif_alt, 0, 0]  # initial velocity  (m/s)
 
-      
+# Añadimos esto para hacer una prueba
 
-T = 2*math.pi*math.sqrt(R**3/mu)
-V_abs = math.sqrt(mu/(R-3000))
+T = 2 * math.pi * math.sqrt(R ** 3 / mu)
+V_abs = math.sqrt(mu / (R - 3000))
 
-a=int(T/2)
+a = int(T / 2)
 
-nframes = 1500               #Free drift until -3000m
-nframes2 = a                  #Hohmann first impulse time: Half period
-nframes3 = a                    #Hohmann second impulse time: Half period in Keeping point
-nframes4 = a                            #Radial impulse from -2900 to -500 m 
-nframes5 = 1300              #
-nframes6 = 2*a
+nframes = 1500  # Free drift until -3000m
+nframes2 = a  # Hohmann first impulse time: Half period
+nframes3 = a  # Hohmann second impulse time: Half period in Keeping point
+nframes4 = a  # Radial impulse from -2900 to -500 m
+nframes5 = 1300  #
+nframes6 = 2 * a
 nframes7 = 0
-
 
 
 xs = []
@@ -46,7 +45,7 @@ xdots = []
 
 for i in range(nframes):
     t = i
-    r_vec= cw.CW2(r0, rdot0, w, t)[0]
+    r_vec = cw.CW2(r0, rdot0, w, t)[0]
     rdot_vec = cw.CW2(r0, rdot0, w, t)[1]
     x = r_vec[0]
     y = r_vec[1]
@@ -55,26 +54,26 @@ for i in range(nframes):
     ydot = rdot_vec[1]
     zdot = rdot_vec[2]
     rdot = [xdot, ydot, zdot]
-    d = math.sqrt(x**2+y**2+z**2)
+    d = math.sqrt(x ** 2 + y ** 2 + z ** 2)
     ds.append(d)
     xs.append(x)
     ys.append(y)
     zs.append(z)
     xdots.append(xdot)
-    v = math.sqrt(xdot**2+ydot**2+zdot**2)
+    v = math.sqrt(xdot ** 2 + ydot ** 2 + zdot ** 2)
 
 V = 0.784443554086188
 
 r0 = [x, y, z]
-deltav = [V , 0, 0]
+deltav = [V, 0, 0]
 
 for i in range(len(rdot)):
-    rdot0[i] = rdot[i]+deltav[i]
+    rdot0[i] = rdot[i] + deltav[i]
 
 
 for i in range(nframes2):
-    t = i 
-    r_vec= cw.CW2(r0, rdot0, w, t)[0]
+    t = i
+    r_vec = cw.CW2(r0, rdot0, w, t)[0]
     rdot_vec = cw.CW2(r0, rdot0, w, t)[1]
     x = r_vec[0]
     y = r_vec[1]
@@ -83,24 +82,24 @@ for i in range(nframes2):
     ydot = rdot_vec[1]
     zdot = rdot_vec[2]
     rdot = [xdot, ydot, zdot]
-    d = math.sqrt(x**2+y**2+z**2)
+    d = math.sqrt(x ** 2 + y ** 2 + z ** 2)
     ds.append(d)
     xs.append(x)
     ys.append(y)
     zs.append(z)
     xdots.append(xdot)
-    
-    v = math.sqrt(xdot**2+ydot**2+zdot**2)
- 
+
+    v = math.sqrt(xdot ** 2 + ydot ** 2 + zdot ** 2)
+
 r0 = [x, y, z]
 deltav = [0.7843611877178878, 0, 0]
 for i in range(len(rdot)):
-    rdot0[i] = rdot[i]+deltav[i]
+    rdot0[i] = rdot[i] + deltav[i]
 
 
 for i in range(nframes3):
-    t = i 
-    r_vec= cw.CW2(r0, rdot0, w, t)[0]
+    t = i
+    r_vec = cw.CW2(r0, rdot0, w, t)[0]
     rdot_vec = cw.CW2(r0, rdot0, w, t)[1]
     x = r_vec[0]
     y = r_vec[1]
@@ -109,32 +108,31 @@ for i in range(nframes3):
     ydot = rdot_vec[1]
     zdot = rdot_vec[2]
     rdot = [xdot, ydot, zdot]
-    d = math.sqrt(x**2+y**2+z**2)
+    d = math.sqrt(x ** 2 + y ** 2 + z ** 2)
     ds.append(d)
     xs.append(x)
     ys.append(y)
     zs.append(z)
     xdots.append(xdot)
 
-    
-    v = math.sqrt(xdot**2+ydot**2+zdot**2)
+    v = math.sqrt(xdot ** 2 + ydot ** 2 + zdot ** 2)
 
 Vx = 0.3
 xf = -500
 xi = r_vec[0]
 var_x = xf - xi
-var_v_z = w* var_x * 0.25
+var_v_z = w * var_x * 0.25
 
 
 r0 = [x, y, z]
 deltav = [0, 0, var_v_z]
 for i in range(len(rdot)):
-    rdot0[i] = rdot[i]+deltav[i]
+    rdot0[i] = rdot[i] + deltav[i]
 
 
 for i in range(nframes4):
-    t = i 
-    r_vec= cw.CW2(r0, rdot0, w, t)[0]
+    t = i
+    r_vec = cw.CW2(r0, rdot0, w, t)[0]
     rdot_vec = cw.CW2(r0, rdot0, w, t)[1]
     x = r_vec[0]
     y = r_vec[1]
@@ -143,27 +141,25 @@ for i in range(nframes4):
     ydot = rdot_vec[1]
     zdot = rdot_vec[2]
     rdot = [xdot, ydot, zdot]
-    d = math.sqrt(x**2+y**2+z**2)
+    d = math.sqrt(x ** 2 + y ** 2 + z ** 2)
     ds.append(d)
     xs.append(x)
     ys.append(y)
     zs.append(z)
     xdots.append(xdot)
 
-    
-    v = math.sqrt(xdot**2+ydot**2+zdot**2)
-    
-    
+    v = math.sqrt(xdot ** 2 + ydot ** 2 + zdot ** 2)
+
 
 r0 = [x, y, z]
 deltav = [0, 0, var_v_z]
 for i in range(len(rdot)):
-    rdot0[i] = rdot[i]+deltav[i]
+    rdot0[i] = rdot[i] + deltav[i]
 
 
 for i in range(nframes4):
-    t = i 
-    r_vec= cw.CW2(r0, rdot0, w, t)[0]
+    t = i
+    r_vec = cw.CW2(r0, rdot0, w, t)[0]
     rdot_vec = cw.CW2(r0, rdot0, w, t)[1]
     x = r_vec[0]
     y = r_vec[1]
@@ -172,34 +168,32 @@ for i in range(nframes4):
     ydot = rdot_vec[1]
     zdot = rdot_vec[2]
     rdot = [xdot, ydot, zdot]
-    d = math.sqrt(x**2+y**2+z**2)
+    d = math.sqrt(x ** 2 + y ** 2 + z ** 2)
     ds.append(d)
     xs.append(x)
     ys.append(y)
     zs.append(z)
     xdots.append(xdot)
 
-    
-    v = math.sqrt(xdot**2+ydot**2+zdot**2)
+    v = math.sqrt(xdot ** 2 + ydot ** 2 + zdot ** 2)
 
 
 Vx = 0.3
 xf = -100
-t1 = (xf-x)/Vx
+t1 = (xf - x) / Vx
 
 
 r0 = [x, y, z]
 deltav = [0.3, 0, 0]
 for i in range(len(rdot)):
-    rdot0[i] = rdot[i]+deltav[i]
-
+    rdot0[i] = rdot[i] + deltav[i]
 
 
 for i in range(nframes5):
-    t = i  
-    gam = [0, 0, 2*w*Vx] 
-    #ESTO ES PARA EL FORCED MOTION EN X CUANDO ESTAMOS EN Z0
-    r_vec= cw.CW(r0, rdot0, w, t, gam)[0]
+    t = i
+    gam = [0, 0, 2 * w * Vx]
+    # ESTO ES PARA EL FORCED MOTION EN X CUANDO ESTAMOS EN Z0
+    r_vec = cw.CW(r0, rdot0, w, t, gam)[0]
     rdot_vec = cw.CW(r0, rdot0, w, t, gam)[1]
     x = r_vec[0]
     y = r_vec[1]
@@ -208,31 +202,28 @@ for i in range(nframes5):
     ydot = rdot_vec[1]
     zdot = rdot_vec[2]
     rdot = [xdot, ydot, zdot]
-    
-    d = math.sqrt(x**2+y**2+z**2)
+
+    d = math.sqrt(x ** 2 + y ** 2 + z ** 2)
     ds.append(d)
     xs.append(x)
     ys.append(y)
     zs.append(z)
     xdots.append(xdot)
 
-    
-    v = math.sqrt(xdot**2+ydot**2+zdot**2)
-  
-    
-    
+    v = math.sqrt(xdot ** 2 + ydot ** 2 + zdot ** 2)
+
+
 r0 = [x, y, z]
 deltav = [-0.3, 0, 0]
 for i in range(len(rdot)):
-    rdot0[i] = rdot[i]+deltav[i]
-
+    rdot0[i] = rdot[i] + deltav[i]
 
 
 for i in range(nframes5):
-    t = i  
-    gam = [0, 0, 0] 
-    #ESTO ES PARA EL FORCED MOTION EN X CUANDO ESTAMOS EN Z0
-    r_vec= cw.CW(r0, rdot0, w, t, gam)[0]
+    t = i
+    gam = [0, 0, 0]
+    # ESTO ES PARA EL FORCED MOTION EN X CUANDO ESTAMOS EN Z0
+    r_vec = cw.CW(r0, rdot0, w, t, gam)[0]
     rdot_vec = cw.CW(r0, rdot0, w, t, gam)[1]
     x = r_vec[0]
     y = r_vec[1]
@@ -241,32 +232,31 @@ for i in range(nframes5):
     ydot = rdot_vec[1]
     zdot = rdot_vec[2]
     rdot = [xdot, ydot, zdot]
-    
-    d = math.sqrt(x**2+y**2+z**2)
+
+    d = math.sqrt(x ** 2 + y ** 2 + z ** 2)
     ds.append(d)
     xs.append(x)
     ys.append(y)
     zs.append(z)
     xdots.append(xdot)
 
-    
-    v = math.sqrt(xdot**2+ydot**2+zdot**2)
+    v = math.sqrt(xdot ** 2 + ydot ** 2 + zdot ** 2)
 
 xf = 100
 xi = r_vec[0]
 var_x = xf - xi
-var_v_z = w* var_x * 0.25
+var_v_z = w * var_x * 0.25
 
 
 r0 = [x, y, z]
 deltav = [0, 0, var_v_z]
 for i in range(len(rdot)):
-    rdot0[i] = rdot[i]+deltav[i]
+    rdot0[i] = rdot[i] + deltav[i]
 
 
 for i in range(nframes6):
-    t = i 
-    r_vec= cw.CW2(r0, rdot0, w, t)[0]
+    t = i
+    r_vec = cw.CW2(r0, rdot0, w, t)[0]
     rdot_vec = cw.CW2(r0, rdot0, w, t)[1]
     x = r_vec[0]
     y = r_vec[1]
@@ -275,20 +265,15 @@ for i in range(nframes6):
     ydot = rdot_vec[1]
     zdot = rdot_vec[2]
     rdot = [xdot, ydot, zdot]
-    d = math.sqrt(x**2+y**2+z**2)
+    d = math.sqrt(x ** 2 + y ** 2 + z ** 2)
     ds.append(d)
     xs.append(x)
     ys.append(y)
     zs.append(z)
     xdots.append(xdot)
 
-    
-    v = math.sqrt(xdot**2+ydot**2+zdot**2)
-    
-       
-    
-    
-    
+    v = math.sqrt(xdot ** 2 + ydot ** 2 + zdot ** 2)
+
 
 """
 
@@ -326,12 +311,11 @@ for i in range(nframes7):
 
 
 fig = plt.figure()
-plt.plot(xs,zs,'r-')
+plt.plot(xs, zs, "r-")
 plt.gca().invert_xaxis()
 plt.gca().invert_yaxis()
 plt.ylabel("$R$-Bar ($m$)")
 plt.xlabel("$V$-Bar ($m$)")
-#plt.legend()
-plt.grid(linestyle='-')
+# plt.legend()
+plt.grid(linestyle="-")
 plt.title("Relative movement")
-
